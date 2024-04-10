13a61995bc5164050bf5795ffe294886e29b5ef0
Hans Markwart <hans@rapidrabbit.com>
2024-04-10 23:15:34 +0200

    - added compiler to the toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
     }
     packaging {
         resources {
